e502a51b1168f1df182de6099f9a5a2e4f0ca989
Dators08 <niks.leimanis3@gmail.com>
2022-03-10 15:43:21 +0200

    pievienoti papildus auto

--- Assets/Skripti/objektuSkripts.cs
+++ Assets/Skripti/objektuSkripts.cs
@@ -8,6 +8,15 @@ public class objektuSkripts : MonoBehaviour {
 	public GameObject Atkritumi;
 	public GameObject Ambulance;
 	public GameObject Autobuss;
+	public GameObject b2;
+	public GameObject Cements;
+	public GameObject e46;
+	public GameObject ekskavators;
+	public GameObject Policija;
+	public GameObject TraktorsDzeltens;
+	public GameObject TraktorsZals;
+	public GameObject Ugunsdzesejs;
+
 
 	[HideInInspector]
 	public Vector2 atkrKord;
@@ -15,6 +24,22 @@ public class objektuSkripts : MonoBehaviour {
 	public Vector2 ambKord;
 	[HideInInspector]
 	public Vector2 autKord;
+	[HideInInspector]
+	public Vector2 b2Kord;
+	[HideInInspector]
+	public Vector2 cemKord;
+	[HideInInspector]
+	public Vector2 e46Kord;
+	[HideInInspector]
+	public Vector2 eksKord;
+	[HideInInspector]
+	public Vector2 polKord;
+	[HideInInspector]
+	public Vector2 traktDzKord;
+	[HideInInspector]
+	public Vector2 traktZKord;
+	[HideInInspector]
+	public Vector2 UgunsKord;
 
 	public AudioSource skanasAvots;
 	public AudioClip[] skana;
@@ -28,5 +53,13 @@ public class objektuSkripts : MonoBehaviour {
 		atkrKord = Atkritumi.GetComponent<RectTransform> ().localPosition;
 		ambKord = Ambulance.GetComponent<RectTransform> ().localPosition;
 		autKord = Autobuss.GetComponent<RectTransform> ().localPosition;
+		b2Kord = b2.GetComponent<RectTransform> ().localPosition;
+		cemKord = Cements.GetComponent<RectTransform> ().localPosition;
+		e46Kord = e46.GetComponent<RectTransform> ().localPosition;
+		eksKord = ekskavators.GetComponent<RectTransform> ().localPosition;
+		polKord = Policija.GetComponent<RectTransform> ().localPosition;
+		traktDzKord = TraktorsDzeltens.GetComponent<RectTransform> ().localPosition;
+		traktZKord = TraktorsZals.GetComponent<RectTransform> ().localPosition;
+		UgunsKord = Ugunsdzesejs.GetComponent<RectTransform> ().localPosition;
 	}
 }
